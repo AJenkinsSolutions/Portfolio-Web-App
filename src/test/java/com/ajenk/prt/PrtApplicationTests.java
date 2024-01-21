ceea5b2bda946cac9274231d6596fbffbe67d683
AJenkinsSolutions <ajenkinsuiw@gmail.com>
2024-01-21 17:34:39 -0600

    First commit

---
+++ src/test/java/com/ajenk/prt/PrtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajenk.prt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
